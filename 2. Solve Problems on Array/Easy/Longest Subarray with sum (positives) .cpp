48ee37663d8166100ac07f53b02d504a8752061c
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-08-01 00:34:04 +0530

    Create Longest Subarray with sum (positives) .cpp

---
+++ 2. Solve Problems on Array/Easy/Longest Subarray with sum (positives) .cpp
@@ -0,0 +1,58 @@
+// BruteForce Approach:
+
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+        for (int j = i; j < n; j++) { // ending index
+            // add all the elements of
+            // subarray = a[i...j]:
+            long long s = 0;
+            for (int K = i; K <= j; K++) {
+                s += a[K];
+            }
+
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+    }
+    return len;
+}
+
+// T.C --> O(N3) approx., where N = size of the array.  Reason: We are using three nested loops, each running approximately N times.
+// S.C --> O(1) as we are not using any extra space. 
+
+
+// Optimal Approach (two pointers):
+
+int getLongestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+
+    int left = 0, right = 0; // 2 pointers
+    long long sum = a[0];
+    int maxLen = 0;
+    while (right < n) {
+        // if sum > k, reduce the subarray from left
+        // until sum becomes less or equal to k:
+        while (left <= right && sum > k) {
+            sum -= a[left];
+            left++;
+        }
+
+        // if sum = k, update the maxLen i.e. answer:
+        if (sum == k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        // Move forward thw right pointer:
+        right++;
+        if (right < n) sum += a[right];
+    }
+
+    return maxLen;
+}
+
+
+// T.C --> O(2*N), where N = size of the given array. Reason: The outer while loop i.e. the right pointer can move up to index n-1(the last index). Now, the inner while loop i.e. the left pointer can move up to the right pointer at most. So, every time the inner loop does not run for n times rather it can run for n times in total. So, the time complexity will be O(2*N) instead of O(N2).
+// S.C --> O(1) as we are not using any extra space.
