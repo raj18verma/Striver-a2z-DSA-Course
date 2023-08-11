383fce26e564a4a1ae7e8f6e520b05cac78f7a3d
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-08-12 00:08:27 +0530

    Create Implement Upper Bound.cpp

---
+++ 3. Binary Search/BS on 1d Arrays/Implement Upper Bound.cpp
@@ -0,0 +1,33 @@
+// BRUTEFORCE APPROACH (Linear Search)
+
+int upperBound(vector<int> &arr, int x, int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > x) {
+            // upper bound found:
+            return i;
+        }
+    }
+    return n;
+}
+
+
+// Optimal Approach (Binary Search)
+
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
