fd0dddad1d8124c779d031abc78e3b56b25aa68e
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-09-02 20:21:10 +0530

    Update Split Array.cpp

--- 3. Binary Search/BS on Answers/Split Array.cpp
+++ 3. Binary Search/BS on Answers/Split Array.cpp
@@ -36,6 +36,40 @@ int largestSubarraySumMinimized(vector<int> a, int k) {
 
 /* INTUITION: Same as book allocation */
 
+int countPartitions(vector<int> &a, int maxSum) {
+    int n = a.size(); //size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++) {
+        if (subarraySum + a[i] <= maxSum) {
+            //insert element to current subarray
+            subarraySum += a[i];
+        }
+        else {
+            //insert element to next subarray
+            partitions++;
+            subarraySum = a[i];
+        }
+    }
+    return partitions;
+}
 
+int largestSubarraySumMinimized(vector<int> a, int k) {
+    // Write Your Code Here
+    int low = *max_element(a.begin(), a.end());
+    int high = accumulate(a.begin(), a.end(), 0);
+    //Apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int partitions = countPartitions(a, mid);
+        if (partitions > k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
 
 // Time and Space Complexity (same as book allocation)
