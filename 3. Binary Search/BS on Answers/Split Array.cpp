82c15775ae82394a2fcff0ba8d4e209d7cecf86e
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-09-02 20:16:41 +0530

    Create Split Array.cpp

---
+++ 3. Binary Search/BS on Answers/Split Array.cpp
@@ -0,0 +1,41 @@
+// BruteForce Approach
+
+/* Intuition: Same as book allocation */
+
+int countPartitions(vector<int> &a, int maxSum) {
+    int n = a.size(); //size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++) {
+        if (subarraySum + a[i] <= maxSum) {
+            //insert element to current subarray
+            subarraySum += a[i];
+        }
+        else {
+            //insert element to next subarray
+            partitions++;
+            subarraySum = a[i];
+        }
+    }
+    return partitions;
+}
+
+int largestSubarraySumMinimized(vector<int> a, int k) {
+    // Write Your Code Here
+    int low = *max_element(a.begin(), a.end());
+    int high = accumulate(a.begin(), a.end(), 0);
+
+    for (int maxSum = low; maxSum <= high; maxSum++) {
+        if (countPartitions(a, maxSum) == k)
+            return maxSum;
+    }
+    return low;
+}
+
+// Optimal Approach
+
+/* INTUITION: Same as book allocation */
+
+
+
+// Time and Space Complexity (same as book allocation)
