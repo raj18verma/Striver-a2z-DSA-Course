b25ef48c1bdf059fb3c579a764e9d9a343310802
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-09-02 19:38:35 +0530

    Create Aggressive Cows.cpp

---
+++ 3. Binary Search/BS on Answers/Aggressive Cows.cpp
@@ -0,0 +1,73 @@
+// BruteForce Approach
+
+/* Intuition: The extremely naive approach is to check all possible distances from 1 to max(stalls[])-min(stalls[]). The maximum distance for which the function canWePlace() returns true, will be our answer. */
+
+bool canWePlace(vector<int> &stalls, int dist, int cows) {
+    int n = stalls.size(); //size of array
+    int cntCows = 1; //no. of cows placed
+    int last = stalls[0]; //position of last placed cow.
+    for (int i = 1; i < n; i++) {
+        if (stalls[i] - last >= dist) {
+            cntCows++; //place next cow.
+            last = stalls[i]; //update the last location.
+        }
+        if (cntCows >= cows) return true;
+    }
+    return false;
+}
+int aggressiveCows(vector<int> &stalls, int k) {
+    int n = stalls.size(); //size of array
+    //sort the stalls[]:
+    sort(stalls.begin(), stalls.end());
+
+    int limit = stalls[n - 1] - stalls[0];
+    for (int i = 1; i <= limit; i++) {
+        if (canWePlace(stalls, i, k) == false) {
+            return (i - 1);
+        }
+    }
+    return limit;
+}
+
+// Time Complexity: O(max-min) * O(n)
+// Space Complexity: O(1)
+
+
+
+// Optimal Approach (Binary Search)
+
+/* Intution: The primary objective of the Binary Search algorithm is to efficiently determine the appropriate half to eliminate, thereby reducing the search space by half. It does this by determining a specific condition that ensures that the target is not present in that half.
+Upon closer observation, we can recognize that our answer space, represented as [1, (max(stalls[])-min(stalls[]))], is actually sorted. Additionally, we can identify a pattern that allows us to divide this space into two halves: one consisting of potential answers and the other of non-viable options. So, we will apply binary search on the answer space. */
+
+bool canWePlace(vector<int> &stalls, int dist, int cows) {
+    int n = stalls.size(); //size of array
+    int cntCows = 1; //no. of cows placed
+    int last = stalls[0]; //position of last placed cow.
+    for (int i = 1; i < n; i++) {
+        if (stalls[i] - last >= dist) {
+            cntCows++; //place next cow.
+            last = stalls[i]; //update the last location.
+        }
+        if (cntCows >= cows) return true;
+    }
+    return false;
+}
+int aggressiveCows(vector<int> &stalls, int k) {
+    int n = stalls.size(); //size of array
+    //sort the stalls[]:
+    sort(stalls.begin(), stalls.end());
+
+    int low = 1, high = stalls[n - 1] - stalls[0];
+    //apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (canWePlace(stalls, mid, k) == true) {
+            low = mid + 1;
+        }
+        else high = mid - 1;
+    }
+    return high;
+}
+
+// Time Complexity: O(NlogN) + O(N * log(max(stalls[])-min(stalls[])))
+// Space Complexity: O(1)
