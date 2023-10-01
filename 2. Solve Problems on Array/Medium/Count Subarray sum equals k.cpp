159e95e1cc8ca17305677140cf5eaeefeeb6b4ab
RAJ VERMA <104757394+raj18verma@users.noreply.github.com>
2023-10-01 22:51:50 +0530

    Create Count Subarray sum equals k.cpp

---
+++ 2. Solve Problems on Array/Medium/Count Subarray sum equals k.cpp
@@ -0,0 +1,81 @@
+// BruteForce Approach:
+
+/* INTUITION: Generate all subarrays and check the count of no. of subarrays sum equals k. */
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        for (int j = i; j < n; j++) { // ending index j
+
+            // calculate the sum of subarray [i...j]
+            int sum = 0;
+            for (int K = i; K <= j; K++)
+                sum += arr[K];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+// T.C:- O(N3) and S.C:- O(1)
+
+
+// Better Approach:
+
+/* INTUITION: Handle the third loop to reduce time complexity */
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    int cnt = 0; // Number of subarrays:
+
+    for (int i = 0 ; i < n; i++) { // starting index i
+        int sum = 0;
+        for (int j = i; j < n; j++) { // ending index j
+            // calculate the sum of subarray [i...j]
+            // sum of [i..j-1] + arr[j]
+            sum += arr[j];
+
+            // Increase the count if sum == k:
+            if (sum == k)
+                cnt++;
+        }
+    }
+    return cnt;
+}
+
+// T.C:-O(N2)    S.C:- O(1)
+
+
+// Optimal Appraoch
+
+/* Intuition: Prefix Sum and Hashmap */
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    map mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; // Setting 0 in the map.
+    for (int i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += arr[i];
+
+        // Calculate x-k:
+        int remove = preSum - k;
+
+        // Add the number of subarrays to be removed:
+        cnt += mpp[remove];
+
+        // Update the count of prefix sum
+        // in the map.
+        mpp[preSum] += 1;
+    }
+    return cnt;
+}
+
+// T.C:-O(N) or O(NLOGN)  S.C:- O(n)
